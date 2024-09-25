61d3cfd82783ba18a086e45c6c97b895c95edebd
yashdark01 <137425534+yashdark01@users.noreply.github.com>
2024-09-25 15:45:21 +0530

    update

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,26 +1,31 @@
-import React from 'react';
-import logo from '../../public/image262.png'
+import React from "react";
+import logo from "../../public/image262.png";
 
 const Footer = () => {
   return (
     <div className="bg-black/75  relative z-0 bottom-0 min-w-full  text-white py-12">
-      <div className="container relative z-0  mx-auto py-36 px-6 md:px-12 lg:px-24 grid gap-[10%] grid-cols-1 md:grid-cols-3 ">
-        
+      <div className="container relative z-0 overflow-hidden mx-auto py-24 ssm:py-28 msm:py-36 px-6 md:px-12 lg:px-24 grid gap-[10%] grid-cols-1 md:grid-cols-3 ">
         {/* Logo and Description Section */}
-        <div>
-          <img 
+        <div className="h-[300px] overflow-hidden">
+          {" "}
+          {/* Set constant height */}
+          <img
             src={logo}
-            alt="Smart National Solutions Logo" 
+            alt="Smart National Solutions Logo"
             className="h-24 mb-8"
           />
-          <p className=" text-lg leading-relaxed">
-            Smart National Solutions (“SNS”) is a leading Saudi Company that provides technology and business consulting services ranging from advisory, systems integration, data services, and support.
+          <p className="text-lg leading-relaxed">
+            Smart National Solutions (“SNS”) is a leading Saudi Company that
+            provides technology and business consulting services ranging from
+            advisory, systems integration, data services, and support.
           </p>
         </div>
 
         {/* Services Section */}
-        <div>
-          <h4 className=" font-bold text-3xl mb-4">Services</h4>
+        <div className="h-[300px] overflow-hidden">
+          {" "}
+          {/* Set constant height */}
+          <h4 className="font-bold text-3xl mb-4">Services</h4>
           <ul className="text-xl space-y-5">
             <li>Blogs</li>
             <li>Data Services</li>
@@ -31,9 +36,11 @@ const Footer = () => {
         </div>
 
         {/* Solutions Section */}
-        <div>
-          <h4 className=" font-bold text-3xl mb-4">Solutions</h4>
-          <ul className=" text-xl space-y-5">
+        <div className="h-[350px] overflow-hidden">
+          {" "}
+          {/* Set constant height */}
+          <h4 className="font-bold text-3xl mb-4">Solutions</h4>
+          <ul className="text-xl space-y-5">
             <li>ERP</li>
             <li>HR</li>
             <li>Customer Experience</li>
@@ -48,8 +55,8 @@ const Footer = () => {
         <p className="mt-2 underline">
           <a href="#" className=" hover:text-gray-300">
             Privacy & Cookies
-          </a> 
-          &nbsp;|&nbsp; 
+          </a>
+          &nbsp;|&nbsp;
           <a href="#" className=" hover:text-gray-300">
             Terms & Conditions
           </a>
